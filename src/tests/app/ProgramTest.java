16a3c0f3e3cc0ec1230360620dd3e5f65f0c80d8
Jordan <jordanrafael92@gmail.com>
2021-10-08 19:06:07 -0300

    Created Test JUnit - Create Object When Valid Data

---
+++ src/tests/app/ProgramTest.java
@@ -0,0 +1,19 @@
+package tests.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Financing;
+
+class ProgramTest {
+
+	@Test
+	public void createObjectCorrectData() {
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		
+		Assertions.assertEquals(100000.0, f.getTotalAmount());
+		Assertions.assertEquals(2000.0, f.getIncome());
+		Assertions.assertEquals(80, f.getMonths());
+	}
+
+}
